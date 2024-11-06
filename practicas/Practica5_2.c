19549109602dce2a6d9fb088f057a7035d45d83c
danitrapero10 <danitrape@gmail.com>
2024-11-06 11:29:39 +0100

    2

--- practicas/Practica5_2.c
+++ practicas/Practica5_2.c
@@ -5,39 +5,41 @@
  *	Autores: Samuel Carías y Daniel Trapero
  * 
  */
-
+#define APROBADO 5.0
 struct notas{
 	char nombre [40];
-	float notas_finales;
+	double notas_finales;
 };
 
 
 int main (){
 	int num_estudiantes = 0;
+	int aprobados = 0;
+	int suspensos = 0;
 	float suma = 0;	
 	float promedio = 0;	
-	struct notas Estudiantes[200];
-	printf("Introduce el nombre y la nota final de cada estudiante: \n");
-	scanf(" %d",&num_estudiantes);
+		printf("Introduce el numero de estudiantes: \n");
+		scanf(" %d",&num_estudiantes);
+	struct notas Estudiantes[num_estudiantes];
 	for (int i = 0; i < num_estudiantes; i++){
-	printf("Nombre del estudiante: ");
-	scanf(" %s",Estudiantes[i].nombre);
-	printf("Nota final del estudiante: ");
-	scanf(" %f",&Estudiantes[i].notas_finales);
+		printf("Nombre del estudiante: ");
+		scanf(" %s",Estudiantes[i].nombre);
+		printf("Nota final del estudiante: ");
+		scanf(" %lf",&Estudiantes[i].notas_finales);
+			if (Estudiantes[i].notas_finales >= APROBADO){
+				aprobados = aprobados + 1;
+		}else{
+			suspensos = suspensos + 1;
+		}
 
-	suma += Estudiantes[i].notas_finales;
+		suma += Estudiantes[i].notas_finales;
 	
 	}
 
 	promedio = suma/num_estudiantes;
 	printf("Nota promedio de la clase: %f\n",promedio);
-
-#define Limite_nota_aprobado 5.0
-struct notas aprobadas = 10;
-for (int i =0, ){
-	printf("La nota es:", &nota);
-	scanf (
-
+	printf("El número de alumnos aprobados es: %d\n", aprobados);
+	printf("El número de alumnos suspensos es: %d\n", suspensos);
 
 return EXIT_SUCCESS;
 }
