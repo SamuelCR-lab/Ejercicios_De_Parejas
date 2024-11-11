66695b8cd8887c9ba0812420836d85865069e214
SamuelCR-lab <fp.scariasramos@salesianosdosa.com>
2024-11-11 20:39:15 +0100

    practica 3 casi terminada

--- practicas/practica5_3.c
+++ practicas/practica5_3.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
  *
  *	Autores: Samuel Carías y Daniel Trapero
  * 
  */
-#define APROBADO 5.0
+#define BAJO_STOCK 5.0
 struct inventario{
 	char nombre [20];
 	int cantidad;
@@ -17,11 +17,12 @@ int main (){
 	int productos = 0;
 	int contador = 0;
 	int con_inventario = 0;
-	char reabastecer [3];
+	char reabastecer[3];
+	char comparacion[3] = "si";
 	char nombre [20];	
 	int aniadir = 0;
-	int posicion_max=0;
-	int posicion=0;
+	char buscar [20];
+	int resultado;
 	printf("Ingrese el número total de productos que desea gestionar (máximo 10): ");
 	scanf("%d",&productos);
 	struct inventario tienda[productos];
@@ -36,21 +37,33 @@ int main (){
 		printf("Producto %d: %s - %d unidades\n",++con_inventario,tienda[i].nombre,tienda[i].cantidad);
 	}
 	for (int i = 0; i < productos; i++){
-		printf("¿Desea reabastecer algún producto? (sí/no):");
+		printf("¿Desea reabastecer algún producto? (si/no): ");
 		scanf("%s",reabastecer);
-		if (reabastecer == "no"){
-			return 0;
-		}
+		resultado = strcmp(reabastecer, comparacion);
+		if (resultado = '0'){
 		printf("Ingrese el nombre del producto: ");
 		scanf("%s",nombre);
-		if (nombre == tienda[i].nombre){
-		printf("Cantidad a añadir: ");
-		scanf("%d",&aniadir);
-		tienda[i].cantidad = tienda[i].cantidad + aniadir;
-		continue;
+			if (strcmp(nombre, tienda[i].nombre)){
+			printf("Cantidad a añadir: ");
+			scanf("%d",&aniadir);
+			tienda[i].cantidad += aniadir;
+			}
+		}else{
+			return 1;
 		}
-
 	}	
+	for (int i = 0; i < productos; i++){
+		if (tienda[i].cantidad <= BAJO_STOCK){
+			printf("%s - %d",tienda[i].nombre,tienda[i].cantidad);
+		}
+	}
+	for (int i = 0; i < productos; i++){
+		printf("Ingrese el nombre del producto a buscar:: ");
+		scanf("%s",buscar);
+		if (strcmp(buscar, tienda[i].nombre)){
+			printf("Cantidad en stock: %d unidades",tienda[i].cantidad);
+		}
+	}
 
 return EXIT_SUCCESS;
 }
