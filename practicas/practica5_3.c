7d671e939949cc3ddb41ade95220ca713eb9972f
SamuelACR <samuelcarias1@gmail.com>
2024-11-12 13:43:42 +0100

    Practica 3 esta a punto de terminar

--- practicas/practica5_3.c
+++ practicas/practica5_3.c
@@ -8,7 +8,7 @@
  */
 #define BAJO_STOCK 5.0
 struct inventario{
-	char nombre [20];
+	char nombre[20];
 	int cantidad;
 };
 
@@ -19,10 +19,12 @@ int main (){
 	int con_inventario = 0;
 	char reabastecer[3];
 	char comparacion[3] = "si";
-	char nombre [20];	
+	char nombre1[20];	
 	int aniadir = 0;
-	char buscar [20];
+	char buscar[20];
 	int resultado;
+	int resultado1;
+	int resultado2;
 	printf("Ingrese el número total de productos que desea gestionar (máximo 10): ");
 	scanf("%d",&productos);
 	struct inventario tienda[productos];
@@ -40,31 +42,46 @@ int main (){
 		printf("¿Desea reabastecer algún producto? (si/no): ");
 		scanf("%s",reabastecer);
 		resultado = strcmp(reabastecer, comparacion);
-		if (resultado = '0'){
-		printf("Ingrese el nombre del producto: ");
-		scanf("%s",nombre);
-			if (strcmp(nombre, tienda[i].nombre)){
+	if (resultado == 0){
+			printf("Ingrese el nombre del producto: ");
+			scanf("%s",nombre1);
+			resultado1 = strcmp(nombre1, tienda[i].nombre);
+			if (resultado1 == 0){
 			printf("Cantidad a añadir: ");
 			scanf("%d",&aniadir);
-			tienda[i].cantidad += aniadir;
+			tienda[i].cantidad = tienda[i].cantidad + aniadir;
 			}
-		}else{
-			return 1;
-		}
-	}	
-	for (int i = 0; i < productos; i++){
-		if (tienda[i].cantidad <= BAJO_STOCK){
-			printf("%s - %d",tienda[i].nombre,tienda[i].cantidad);
+		
+		for (int i = 0; i < productos; i++){
+			if (tienda[i].cantidad <= BAJO_STOCK){
+			printf("%s - %d\n",tienda[i].nombre,tienda[i].cantidad);
+				}
+			}	
+		for (int i = 0; i < 1; i++){
+			printf("Ingrese el nombre del producto a buscar: ");
+			scanf("%s",buscar);
+			resultado1 = strcmp(buscar, tienda[i].nombre);
+			if (resultado2 == 0){
+			printf("Cantidad en stock: %d unidades\n",tienda[i].cantidad);
+				}
+			}		
+	}else{
+		for (int i = 0; i < producto; i++){
+			if (tienda[i].cantidad <= BAJO_STOCK){
+			printf("%s - %d\n",tienda[i].nombre,tienda[i].cantidad);
+				}
+			}		
+		for (int i = 0; i < 1; i++){
+			printf("Ingrese el nombre del producto a buscar: ");
+			scanf("%s",buscar);
+			resultado2 = strcmp(buscar, tienda[i].nombre);
+			if (resultado2 == 0){
+			printf("Cantidad en stock: %d unidades\n",tienda[i].cantidad);
+				}
+			}
+		return 0;	
 		}
 	}
-	for (int i = 0; i < productos; i++){
-		printf("Ingrese el nombre del producto a buscar:: ");
-		scanf("%s",buscar);
-		if (strcmp(buscar, tienda[i].nombre)){
-			printf("Cantidad en stock: %d unidades",tienda[i].cantidad);
-		}
-	}
-
 return EXIT_SUCCESS;
 }
 
